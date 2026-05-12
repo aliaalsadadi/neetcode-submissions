2ed4e4d6a7863a9dd929150b25efdb4e3ff47588
Ali Alaa Abdulnabi Alsadadi <126590042+aliaalsadadi@users.noreply.github.com>
2026-05-12 13:41:17 +0300

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private: vector<vector<int>> memo;
+public:
+    int uniquePaths(int m, int n) {
+        memo.resize(m, vector(n, -1));
+        return dfs(0,0,m,n);
+    }
+    int dfs(int i, int j , int m, int n) {
+        if (i>=m || j >= n) {
+            return 0;
+        }
+        if (i == m-1 && j == n-1) {
+            return 1;
+        }
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+         return memo[i][j] = dfs(i+1, j, m ,n) + dfs(i,j+1,m,n);
+    }
+};
